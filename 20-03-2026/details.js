2cb6c627e2ff84937dfb900875b863a4a42a35d9
gowsalyabca18-dotcom <gowsalyabca18@gmail.com>
2026-03-20 09:57:50 +0530

    Add files via upload

---
+++ 20-03-2026/details.js
@@ -0,0 +1,18 @@
+var name="Gowsalya";
+const age=28;
+let isstudent=true;
+let favouriteColour="White";
+console.log(name);
+console.log(typeof(name));
+
+console.log(age);
+console.log(typeof(age));
+
+console.log(isstudent);
+console.log(typeof(isstudent));
+
+console.log(favouriteColour);
+console.log(typeof(favouriteColour));
+
+
+
